e446e04c30f3f446137715ca9be53cbe2faca678
user name <shaynes-k8support@eidf029-host1.vms.os.eidf.epcc.ed.ac.uk>
2023-12-01 14:05:17 +0000

    Fix conversion of labels to tensor

--- code/main.py
+++ code/main.py
@@ -9,23 +9,22 @@ from torchvision.transforms.v2 import Resize, Lambda
 
 class ImagenetDataset(Dataset):
     def __init__(self, annotations_file, img_dir):
-        self.img_labels = pd.read_csv(annotations_file, sep='\t')
+        self.img_labels_raw = pd.read_csv(annotations_file, sep='\t')
+	self.img_labels = torch.IntTensor(numpy.array(pd.get_dummies(self.img_labels_raw.iloc[,1])))
         self.img_dir = img_dir
-        self.transform = Resize([300,300])
-        self.target_transform = Lambda(lambda y: torch.IntTensor(numpy.array(pd.get_dummies(y))))
+        self.transform = Resize([300,300], antialias=True)
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        img_path = os.path.join(self.img_dir, self.img_labels_raw.iloc[idx, 0])
         image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        label = self.img_labels[idx]
         image = self.transform(image)
-        label = self.target_transform(label)
         return image, label
 
-training_data = ImagenetDataset(annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/image_labels.txt", 
+training_data = ImagenetDataset(annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/image_labels.txt",
 				img_dir = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/")
 
 train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
@@ -33,7 +32,7 @@ train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
 # Display image and label.
 train_features, train_labels = next(iter(train_dataloader))
 print(f"Feature batch shape: {train_features.size()}")
-#print(f"Labels batch shape: {train_labels.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
 img = train_features[0].squeeze()
 label = train_labels[0]
 print(f"Label: {label}")

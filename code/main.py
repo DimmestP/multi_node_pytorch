0dcc560b4ded1d450d59fdab649539bee31da51b
user name <shaynes-k8support@eidf029-host1.vms.os.eidf.epcc.ed.ac.uk>
2023-12-01 14:14:01 +0000

    Fix whitespace issue

--- code/main.py
+++ code/main.py
@@ -8,21 +8,21 @@ from torch.utils.data import Dataset
 from torchvision.transforms.v2 import Resize, Lambda
 
 class ImagenetDataset(Dataset):
-    def __init__(self, annotations_file, img_dir):
-        self.img_labels_raw = pd.read_csv(annotations_file, sep='\t')
-	self.img_labels = torch.IntTensor(numpy.array(pd.get_dummies(self.img_labels_raw.iloc[,1])))
-        self.img_dir = img_dir
-        self.transform = Resize([300,300], antialias=True)
+	def __init__(self, annotations_file, img_dir):
+		self.img_labels_raw = pd.read_csv(annotations_file, sep='\t')
+		self.img_labels = torch.IntTensor(numpy.array(pd.get_dummies(self.img_labels_raw.iloc[,1])))
+		self.img_dir = img_dir
+		self.transform = Resize([300,300], antialias=True)
 
-    def __len__(self):
-        return len(self.img_labels)
+	def __len__(self):
+		return len(self.img_labels)
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels_raw.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels[idx]
-        image = self.transform(image)
-        return image, label
+	def __getitem__(self, idx):
+		img_path = os.path.join(self.img_dir, self.img_labels_raw.iloc[idx, 0])
+		image = read_image(img_path)
+		label = self.img_labels[idx]
+		image = self.transform(image)
+		return image, label
 
 training_data = ImagenetDataset(annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/image_labels.txt",
 				img_dir = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/")

d19f70b7679ecb8d6c3f8b07b25489d16849f61e
user name <shaynes-k8support@eidf029-host1.vms.os.eidf.epcc.ed.ac.uk>
2023-12-11 15:24:36 +0000

    Rewrite validation label file without greyscale

--- code/remove_greyscale_images.py
+++ code/remove_greyscale_images.py
@@ -8,27 +8,27 @@ from torchvision.io import read_image
 annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/image_labels.txt"
 img_dir = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/"
 
-img_labels = pd.read_csv(annotations_file, sep='\t')
+img_labels = pd.read_csv(annotations_file, sep='\t', header=None)
 
-for i in range(1,len(img_labels)):
-	img_path = os.path.join(img_dir, img_labels.iloc[i, 0])
-	image = read_image(img_path)
-	if image.size()[0] != 3:
-		os.remove(img_path)
+for i in range(0,(len(img_labels)-1)):
+        img_path = os.path.join(img_dir, img_labels.iloc[i, 0])
+        image = read_image(img_path)
+        if image.size()[0] != 3:
+                os.remove(img_path)
 
 # Remove grey scale validation images
 
 annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/validation/image_labels.txt"
 img_dir = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/validation/"
 
-img_labels = pd.read_csv(annotations_file, sep='\t')
+img_labels = pd.read_csv(annotations_file, sep='\t', header=None)
 
 greyscale_images = []
 
-for i in range(1,len(img_labels)):
+for i in range(0,(len(img_labels)-1)):
         img_path = os.path.join(img_dir, img_labels.iloc[i, 0])
         image = read_image(img_path)
         if image.size()[0] != 3:
                 greyscale_images.append(i)
 
-print(image_labels)
+img_labels.drop(index=greyscale_images).to_csv(annotations_file, sep='\t')

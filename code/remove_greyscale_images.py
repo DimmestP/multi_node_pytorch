3a359e418ec4c355a41c1ebaf00278500f6e592e
user name <shaynes-k8support@eidf029-host1.vms.os.eidf.epcc.ed.ac.uk>
2023-12-11 15:12:07 +0000

    Add validation images to remove greyscale code

--- code/remove_greyscale_images.py
+++ code/remove_greyscale_images.py
@@ -1,7 +1,9 @@
 import os
 import torch
-import pandas
+import pandas as pd
 from torchvision.io import read_image
+
+# Remove grey scale training images
 
 annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/image_labels.txt"
 img_dir = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/train/"
@@ -13,3 +15,20 @@ for i in range(1,len(img_labels)):
 	image = read_image(img_path)
 	if image.size()[0] != 3:
 		os.remove(img_path)
+
+# Remove grey scale validation images
+
+annotations_file = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/validation/image_labels.txt"
+img_dir = "/mnt/ceph_rbd/imagenet_data/imagenet_data_subset/validation/"
+
+img_labels = pd.read_csv(annotations_file, sep='\t')
+
+greyscale_images = []
+
+for i in range(1,len(img_labels)):
+        img_path = os.path.join(img_dir, img_labels.iloc[i, 0])
+        image = read_image(img_path)
+        if image.size()[0] != 3:
+                greyscale_images.append(i)
+
+print(image_labels)
